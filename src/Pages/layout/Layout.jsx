c535a26362fc90a94b3cf09a39221855ce7ded3e
Vishvanath2005 <vishva202005@gmail.com>
2025-03-20 11:55:18 +0530

    completed layout fully responsive

--- src/Pages/layout/Layout.jsx
+++ src/Pages/layout/Layout.jsx
@@ -3,8 +3,9 @@ import Logo from "../../assets/logo.png";
 import { TbLayoutSidebarRightCollapse } from "react-icons/tb";
 import { IoMdMenu } from "react-icons/io";
 import { LuLayoutDashboard } from "react-icons/lu";
-import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
+import { IoMdNotificationsOutline } from "react-icons/io";
 import { GoPeople } from "react-icons/go";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
 import { TbUserQuestion } from "react-icons/tb";
 import { TbTicket } from "react-icons/tb";
 import { PiPackageDuotone } from "react-icons/pi";
@@ -19,9 +20,13 @@ import { NavLink, Outlet, useLocation } from "react-router-dom";
 const Layout = () => {
   const location = useLocation();
   const [open, setOpen] = useState(false);
+  const [hasNotification, setHasNotification] = useState(true);
+
   const toggle = () => {
     setTimeout(1500);
-    setOpen(!open);
+    if (open) {
+      setOpen(!open);
+    }
   };
 
   const Menus = [
@@ -31,8 +36,9 @@ const Layout = () => {
       icon: <HiOutlineBuildingOffice2 />,
       to: "/insitution",
     },
+    { title: "Leads", icon: <GoPeople />, to: "/leads" },
     { title: "Enquires", icon: <TbUserQuestion />, to: "/enquires" },
-    { title: "Tickets", icon: <TbTicket />, to: "/tickets" }, // Main Tickets route
+    { title: "Tickets", icon: <TbTicket />, to: "/tickets" },
     { title: "Packages", icon: <PiPackageDuotone />, to: "/packages" },
     { title: "Users", icon: <FiUserPlus />, to: "/users" },
     { title: "Interview", icon: <TbMessagePlus />, to: "/interview" },
@@ -47,7 +53,7 @@ const Layout = () => {
           className={`${
             !open
               ? ` md:w-32 sm:relative hidden sm:block  `
-              : `w-2/3 absolute  sm:w-3/6   sm:static md:w-72`
+              : `w-2/3 absolute  sm:z-10 sm:w-3/6  sm:static md:w-72`
           }  h-screen z-10 border-r-2 drop-shadow-lg bg-white duration-300`}
         >
           <div
@@ -138,9 +144,50 @@ const Layout = () => {
               </React.Fragment>
             ))}
           </ul>
+          <div
+            className={`${
+              !open ? `left-8` : ` flex w-full justify-center  gap-3`
+            } sm:hidden  fixed bottom-6  justify-center  sm:gap-2.5  items-center`}
+          >
+            <p
+              className={`${
+                open && `text-3xl `
+              }sm:text-2xl sm:hidden relative cursor-pointer  bg-blue-200 sm:px-4 sm:py-2 px-4 py-2   text-blue-800 font-semibold rounded-full`}
+            >
+              V
+              {hasNotification && (
+                <span className=" absolute top-0 right-0 p-1 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></span>
+              )}
+            </p>
+            <div className={`${!open && `hidden`}`}>
+              <p
+                className={`${
+                  open && `text-xl`
+                } sm:text-xl  font-semibold font-Exo`}
+              >
+                Profile Name
+              </p>
+              <p
+                className={`${
+                  open && `text-base`
+                }sm:text-sm  text-gray-400 font-Source Sans Pro`}
+              >
+                Profile Role
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="w-full h-screen overflow-auto bg-slate-100  ">
-          <Navbar open={open} setOpen={setOpen} />
+        <div
+          className={`${
+            open && `sm:blur-none blur-xl  z-0 transition-opacity duration-300`
+          } w-screen h-screen overflow-auto bg-slate-100 `}
+        >
+          <Navbar
+            open={open}
+            setOpen={setOpen}
+            hasnotification={hasNotification}
+            setHasNotification={setHasNotification}
+          />
           <Suspense>
             <Outlet />
           </Suspense>
--- src/Pages/layout/Navbar.jsx
+++ src/Pages/layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoMdMenu } from "react-icons/io";
 import Logo from "../../assets/logo.png";
 
-const Navbar = ({ open, setOpen }) => {
+const Navbar = ({ open, setOpen, hasnotification,sethasnotification }) => {
   return (
     <div className=" mt-5 flex justify-between items-center gap-3 px-3 sm:px-6">
       <span className="flex sm:hidden gap-1.5 justify-center items-center">
@@ -25,7 +25,7 @@ const Navbar = ({ open, setOpen }) => {
         </div>
       </span>
       <div className="font-Source Sans Pro  ">
-        <div className="md:w-96 sm:w-60 w-32 flex gap-1.5 rounded-lg border bg-white sm:p-2  pl-1 items-center  ">
+        <div className="md:w-72 sm:w-60 w-32 flex   gap-1.5 rounded-lg border bg-white sm:p-2  pl-1 items-center  ">
           <IoSearchOutline className="sm:text-xl text-sm text-gray-400 font-semibold" />
           <input
             type="text"
@@ -38,12 +38,19 @@ const Navbar = ({ open, setOpen }) => {
       </div>
 
       <div className="sm:flex sm:gap-5 gap-1 items-center sm:visible hidden ">
-        <div className=" bg-blue-200 sm:p-3 p-1.5 rounded-full">
+        <div className=" sm:hidden lg:block relative cursor-pointer bg-blue-200 sm:p-3 p-1.5 rounded-full">
+           {hasnotification && (
+          <span className=" absolute top-0 right-0 p-1 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></span>
+        )}
           <IoMdNotificationsOutline className="sm:text-2xl  text-lg text-blue-800 " />
+         
         </div>
-        <span className="border border-gray-400 sm:h-12 h-8 "></span>
-        <p className="sm:text-2xl text-base bg-blue-200 sm:px-4 sm:py-2 px-2.5 py-1 text-blue-800 font-semibold rounded-full">
+        <span className=" sm:hidden lg:block border border-gray-400 sm:h-12 h-8 "></span>
+        <p className="sm:text-2xl relative cursor-pointer text-base bg-blue-200 sm:px-4 sm:py-2 px-2.5 py-1 text-blue-800 font-semibold rounded-full">
           V
+          {hasnotification && (
+          <span className="lg:hidden absolute top-0 right-0 p-1 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></span>
+        )}
         </p>
         <div>
           <p className="sm:text-xl text-sm font-semibold font-Exo">
